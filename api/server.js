d2d679f1b1e6a35988674c03148a1acf198b5d4b
mjsa1021 <mjsa1021@gmail.com>
2026-04-03 14:02:29 +0900

    Fix registration and login messages, add find-account

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ let reservations = [];
 // 회원가입
 app.post('/signup', (req, res) => {
     const { id, password, name, birth } = req.body;
-    if (users.find(u => u.id === id)) return res.status(400).send("이미 등록된 번호입니다.");
+    if (users.find(u => u.id === id)) return res.status(400).send("이미 등록된 전화번호입니다.");
     users.push({ id, password, name, birth });
     res.send("회원가입이 완료되었습니다!");
 });
@@ -21,18 +21,25 @@ app.post('/login', (req, res) => {
     const { id, password } = req.body;
     const user = users.find(u => u.id === id && u.password === password);
     if (user) res.send("로그인 성공!");
-    else res.status(401).send("번호나 비밀번호를 확인해주세요.");
+    else res.status(401).send("아이디 또는 비밀번호가 틀렸습니다.");
 });
 
-// 예약 (경산 통합 시스템)
+// 정보 찾기
+app.post('/find-account', (req, res) => {
+    const { name, birth } = req.body;
+    const user = users.find(u => u.name === name && u.birth === birth);
+    if (user) res.send(`정보를 찾았습니다!\n아이디: ${user.id}\n비밀번호: ${user.password}`);
+    else res.status(404).send("정보가 없습니다.");
+});
+
+// 예약하기 (인원 정보 추가)
 app.post('/reserve', (req, res) => {
-    const { userId, date, time, court, people } = req.body;
-    // 중복 확인 (같은 날, 같은 장소, 같은 시간)
-    const isBooked = reservations.find(r => r.date === date && r.time === time && r.court === court);
-    if (isBooked) return res.status(400).send("이미 예약이 꽉 찬 시간대입니다.");
+    const { userId, loc, date, time, court, people } = req.body;
+    const isBooked = reservations.find(r => r.loc === loc && r.date === date && r.time === time && r.court === court);
+    if (isBooked) return res.status(400).send("이미 예약된 시간대입니다.");
     
-    reservations.push({ userId, date, time, court, people });
-    res.send("정상적으로 예약되었습니다.");
+    reservations.push({ userId, loc, date, time, court, people });
+    res.send("예약 완료!");
 });
 
 module.exports = app;
